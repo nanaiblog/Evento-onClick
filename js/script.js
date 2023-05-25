ace013fa35a3b7c6e8bd8aa2b6291ff8913d8553
Nanai <130193531+nanaiblog@users.noreply.github.com>
2023-05-24 20:50:55 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function logOut(element) {
+    element.innerText = "Log Out";
+}
+function quitar(element) {
+    element.remove();
+}
+function mensaje( nombre ) {
+    console.log( "Ninja was liked");
+    alert("Ninja was liked");
+}
